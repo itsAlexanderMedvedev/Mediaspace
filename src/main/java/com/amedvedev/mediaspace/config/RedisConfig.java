3d05f9a7da37e756c19325fe584b30da6d10887c
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-09-05 19:49:05 +0200

    redis added

---
+++ src/main/java/com/amedvedev/mediaspace/config/RedisConfig.java
@@ -0,0 +1,16 @@
+package com.amedvedev.mediaspace.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+}
